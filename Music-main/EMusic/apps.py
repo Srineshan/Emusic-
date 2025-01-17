7d8a6e1cac96884a18cd089ab4ca64b4b4cf036f
Srineshan <151605503+Srineshan@users.noreply.github.com>
2025-01-17 14:35:27 +0530

    Delete Music-main directory

--- Music-main/EMusic/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmusicConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'EMusic'
-    def ready(self):
-        import EMusic.signals
-
